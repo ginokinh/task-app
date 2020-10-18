5147da1a310f10fef799bfe9dae4453c19cce52c
ginokinh <ginokinh@gmail.com>
2020-10-18 14:10:35 +0900

    Only the admin_user can use the user function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,5 @@ class ApplicationController < ActionController::Base
   def login_required
     redirect_to login_path unless current_user
   end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   skip_before_action :login_required
-  
+
   def new
   end
 
   def create
-    user = User.find(email: session_params[:email])
+    user = User.find_by(email: session_params[:email])
     
     if user&.authenticate(session_params[:password])
       # userがnilでない時、authenticateを呼び出す。
